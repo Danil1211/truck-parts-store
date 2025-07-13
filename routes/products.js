665d05607d743ca6f25de52f437499d91549234e
Danil1211 <gluskodanil44@gmail.com>
2025-07-13 20:43:01 +0200

    ліфв

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { Product } = require('./models');
+const { Product } = require('../models');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
