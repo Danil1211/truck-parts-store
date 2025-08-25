83a016c5ca20f98e4206de6e5849f7b99123dedb
Danil1211 <gluskodanil44@gmail.com>
2025-08-25 16:04:29 +0200

    21

--- frontend/src/admin/AdminProductsPage.jsx
+++ frontend/src/admin/AdminProductsPage.jsx
@@ -3,7 +3,7 @@ import React, { useState, useRef, useEffect } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import AdminSubMenu from "./AdminSubMenu";
 import "../assets/AdminPanel.css";
-import api from "./utils/api.js";
+import api from "../utils/api.js";
 
 const BASE_URL = (api.defaults.baseURL || "").replace(/\/+$/, "");
 
